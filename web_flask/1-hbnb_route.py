1bdaa1ad4f7438084f58477f0ebc6b2c9d88000d
KimFarida <farimomoh@gmail.com>
2022-11-13 23:56:40 +0100

    Web Flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application
-0.0.0.0, port 5000 """
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello():
-    """displays Hello HBNB!"""
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """displays HBNB"""
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application
-0.0.0.0, port 5000 """
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello():
-    """displays Hello HBNB!"""
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """displays HBNB"""
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """display 'C' followed by the value of <text>"""
-    text = text.replace('_', ' ')
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
     return "C {}".format(text)
-    """return 'C %s' % text.replace('_', ' ')"""
 
 
 if __name__ == "__main__":
